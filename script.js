c1c05c79a754f409cbd4d37c913817fc7e2057cf
Khalid Hussain <khalidhussainnn@gmail.com>
2022-02-17 02:38:05 +0500

    about section added

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
+$(document).ready(function () {
+  $(".fa-bars").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
+  });
 
+  $(window).on("scroll load", function () {
+    $(".fa-bars").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
 
-
-
-
-
-
-
-$(document).ready(function(){
-    $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
-    $(window).on('scroll load', function(){
-        $('.fa-bars').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if($(window).scrollTop() > 30){
-            $('header').addClass('header-active');
-        }else{
-            $('header').removeClass('header-active');
+    if ($(window).scrollTop() > 30) {
+      $("header").addClass("header-active");
+    } else {
+      $("header").removeClass("header-active");
+    }
+    $('section').each(function(){
+        var top = $(window).scrollTop();
+        var id = $(this).attr('id');
+        var height = $(this).height();
+        var top = $(this).offset().top - 200;
+        if (top >= offset && top < offset + height){
+            $('.navbar').find('')            
         }
-
-
-
     })
-
-
-
-})
+  });
+});
